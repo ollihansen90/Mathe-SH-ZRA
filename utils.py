33ed01e0d8b5b496d5f16a7c8d9db2e56e5417d4
ollihansen90 <50058898+ollihansen90@users.noreply.github.com>
2022-09-29 16:08:59 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def conv1d(data, kernel, pad=(0,0), padmode="reflect"):
     if isinstance(pad, int):
@@ -21,3 +22,8 @@ def conv1d(data, kernel, pad=(0,0), padmode="reflect"):
 def exp_kernel(len, sig=1):
     kernel = np.exp(-(np.arange(0,len)-(len-1)/2)**2/(2*sig**2))
     return kernel/np.sum(kernel)
+
+def get_data():
+    filename = "data.csv"
+    data = pd.read_csv(filename, sep=";")
+    return data["tempmax"].to_numpy()
